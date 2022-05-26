2a69d769a45f0fed735b8bd77bcb4646c2124804
sri1709 <58382463+sri1709@users.noreply.github.com>
2022-05-26 21:49:12 +0530

    Delete PensionerDetail-Microservice directory

--- PensionerDetail-Microservice/src/main/java/com/cts/pensioner/detail/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.cts.pensioner.detail.exception;
-
-@SuppressWarnings("serial")
-public class AuthorizationException extends Exception{
-
-	public AuthorizationException(String message) {
-		super(message);
-	}
-}
